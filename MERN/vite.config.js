ae4395fc1132bff03c621e7fc952a0625f5ac2a0
Faizy426890 <faizanlahori2003@gmail.com>
2024-07-24 01:56:52 +0500

    Fix build output directory

--- MERN/vite.config.js
+++ MERN/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // This should match the directory you see
+    outDir: 'dist' // This should match the directory you are seeing
   }
 });
