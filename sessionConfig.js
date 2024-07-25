27f1c93d259f84f6b00494f27168a95cbf635a68
Faizy426890 <faizanlahori2003@gmail.com>
2024-07-25 15:51:58 +0500

    Fix session configuration for Vercel deployment

--- sessionConfig.js
+++ sessionConfig.js
@@ -2,18 +2,20 @@ import session from 'express-session';
 import MongoStore from 'connect-mongo';
 
 const configureSession = (app) => {
+  const isProduction = process.env.NODE_ENV === 'production'; // Check if the environment is production
   app.use(session({
     secret: process.env.SESSION_SECRET || 'your_session_secret',
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI, // Ensure this is correctly set in environment variables
+      mongoUrl: process.env.MONGODB_URI,
       ttl: 14 * 24 * 60 * 60 // 14 days
     }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS
-      maxAge: 14 * 24 * 60 * 60 * 1000 // 14 days
-    },
+      secure: isProduction, // Set to true if using HTTPS in production
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+      sameSite: isProduction ? 'none' : 'lax' // Handle cross-origin requests
+    }
   }));
 };
 
